cef1ecfb6a85815a9d65eac7b9c55dc13507dbfa
Erdem Can Irmak <rdmcnrmk@gmail.com>
2019-08-17 14:58:19 +0200

    new questions are added
    q.1.1 and q.1.2

---
+++ q11.cpp
@@ -0,0 +1,60 @@
+
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+bool IsUnique(string str){
+  set<char> c_set;
+
+  for (size_t i = 0; i < str.size(); i++) {
+    if(c_set.find(str[i]) == c_set.end()){
+      c_set.insert(str[i]);
+    }
+    else
+      return false;
+  }
+
+  return true;
+}
+
+bool IsUniqueNoSet(string str){
+  sort(str.begin(), str.end());
+
+  for (size_t i = 1; i < str.size(); i++) {
+    if(str[i] == str[i-1])
+      return false;
+  }
+
+  return true;
+}
+
+bool BookSolution(string str){
+  if(str.size() > 128) return false;
+
+  bool char_set[128] = {false};
+  for (size_t i = 0; i < str.size(); i++) {
+    if(char_set[str[i]] == true)
+      return false;
+
+    char_set[str[i]] = true;
+  }
+  return true;
+}
+
+int main(int argc, char const *argv[]) {
+  cout << IsUnique("") << "\n"; // returns true
+  cout << IsUnique("asdfgzxcv") << "\n"; // returns true
+  cout << IsUnique("asdfaasdf") << "\n"; // returns false
+
+  cout << IsUniqueNoSet("") << "\n"; // returns true
+  cout << IsUniqueNoSet("asdfgzxcv") << "\n"; // returns true
+  cout << IsUniqueNoSet("asdfaasdf") << "\n"; // returns false
+
+  cout << BookSolution("") << "\n"; // returns true
+  cout << BookSolution("asdfgzxcv") << "\n"; // returns true
+  cout << BookSolution("asdfaasdf") << "\n"; // returns false
+  return 0;
+}
---
+++ q12.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool CheckPermutation(string str1, string str2){
+  if(str1.size() != str2.size()) return false;
+
+  sort(str1.begin(), str1.end());
+  sort(str2.begin(), str2.end());
+  
+  return str1.compare(str2) == 0;
+}
+
+bool CheckPermutation2(string str1, string str2){
+  if(str1.size() != str2.size()) return false;
+
+  int counts[128] = {0};
+
+  for (size_t i = 0; i < str1.size(); i++) {
+    counts[str1[i]]++;
+  }
+
+  for (size_t i = 0; i < str2.size(); i++) {
+    counts[str2[i]]--;
+
+    if(counts[str2[i]] < 0)
+    return false;
+  }
+
+  return true;
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  cout << CheckPermutation("asdasd", "sadsad") << "\n"; // returns true
+  cout << CheckPermutation("asdfsa", "asgafa") << "\n"; // returns false
+
+  cout << CheckPermutation2("asdasd", "sadsad") << "\n"; // returns true
+  cout << CheckPermutation2("asdfsa", "asgfas") << "\n"; // returns false
+
+  return 0;
+}
